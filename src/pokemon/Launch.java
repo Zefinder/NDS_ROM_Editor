25b79b76ef93974e324ecd49ce3529e2832ae6f6
Zefinder <jakubiak@insa-toulouse.fr>
2025-03-08 11:55:56 +0100

    Added ROM selection

--- src/pokemon/Launch.java
+++ src/pokemon/Launch.java
@@ -1,14 +1,27 @@
 package pokemon;
 
+import java.io.File;
 import java.io.IOException;
+
+import javax.swing.JFileChooser;
 
 import pokemon.frame.MainFrame;
 
 public class Launch {
 
 	public static void main(String[] args) throws IOException {
-		MainFrame frame = new MainFrame("aaa", "C:\\Users\\adric\\Desktop\\pokemon test\\test");
-		frame.initFrame();
+		// Choose the ROM folder
+		JFileChooser chooser = new JFileChooser();
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		int answer = chooser.showOpenDialog(null);
+		if (answer == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = chooser.getSelectedFile();
+			MainFrame frame = new MainFrame(selectedFile.getName(), selectedFile.getPath());
+			frame.initFrame();
+		}
+		
+//		MainFrame frame = new MainFrame("aaa", "C:\\Users\\adric\\Desktop\\pokemon test\\test");
+//		frame.initFrame();
 		
 //		File f = new File("C:\\Users\\adric\\Desktop\\pokemon test\\test\\graphic\\record.narc");
 //		InputStream inStream = new DataInputStream(new FileInputStream(f));
